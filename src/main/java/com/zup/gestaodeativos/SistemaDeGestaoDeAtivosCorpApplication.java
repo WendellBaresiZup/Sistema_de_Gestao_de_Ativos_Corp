ee86909be66916a3105cb41816ed5c4522a12fbe
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-05-12 12:03:50 -0300

    Adicionado o projeto

---
+++ src/main/java/com/zup/gestaodeativos/SistemaDeGestaoDeAtivosCorpApplication.java
@@ -0,0 +1,13 @@
+package com.zup.gestaodeativos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestaoDeAtivosCorpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestaoDeAtivosCorpApplication.class, args);
+	}
+
+}
