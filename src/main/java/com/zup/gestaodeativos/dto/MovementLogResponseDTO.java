21424efe5981916e017c4aa5a9568fdb9c69bf26
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-05-13 16:57:23 -0300

    Feat: Add LogMovimentacaoResponseDTO for output data

--- src/main/java/com/zup/gestaodeativos/dto/MovementLogResponseDTO.java
+++ src/main/java/com/zup/gestaodeativos/dto/MovementLogResponseDTO.java
@@ -1,4 +1,28 @@
 package com.zup.gestaodeativos.dto;
 
+import com.zup.gestaodeativos.models.Active;
+import com.zup.gestaodeativos.models.User;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class MovementLogResponseDTO {
+    private Long id;
+    private Active active;
+    private User user;
+    private LocalDateTime movementDate;
+    private String action;
+
+    public MovementLogResponseDTO (Long id, Active active, User user, LocalDateTime movementDate, String action){
+        this.id = id;
+        this.active = active;
+        this.user = user;
+        this.movementDate = movementDate;
+        this.action = action;
+    }
+
+    public MovementLogResponseDTO(){
+
+    }
 }
