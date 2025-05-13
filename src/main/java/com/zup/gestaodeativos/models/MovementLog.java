8e8b2c00114af616c647033d0f4255ca4e700b19
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-05-13 16:09:11 -0300

    Feat: Add Movemente Log entity with attributes

--- src/main/java/com/zup/gestaodeativos/models/MovementLog.java
+++ src/main/java/com/zup/gestaodeativos/models/MovementLog.java
@@ -1,4 +1,30 @@
 package com.zup.gestaodeativos.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
 public class MovementLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Active active;
+    private User user;
+    private LocalDateTime movementDate;
+    private String action;
+
+    public MovementLog(){
+    }
+
+    public MovementLog(Long id, Active active, User user, LocalDateTime movementDate, String action){
+        this.id = id;
+        this.active = active;
+        this.user = user;
+        this.movementDate = movementDate;
+        this.action = action;
+    }
+
 }
