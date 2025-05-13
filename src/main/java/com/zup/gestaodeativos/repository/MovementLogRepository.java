81a37986fb0f0d78bf02e4a370d1a9d6ff655e86
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-05-13 16:15:57 -0300

    Feat: Add MovementeLogRepository to interact with the database

--- src/main/java/com/zup/gestaodeativos/repository/MovementLogRepository.java
+++ src/main/java/com/zup/gestaodeativos/repository/MovementLogRepository.java
@@ -1,4 +1,10 @@
 package com.zup.gestaodeativos.repository;
 
-public class MovementLogRepository {
+import com.zup.gestaodeativos.models.MovementLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovementLogRepository extends JpaRepository<MovementLog, Long> {
+
 }
