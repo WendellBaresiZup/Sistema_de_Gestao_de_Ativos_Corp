54673b3b6a6fd7c5b0a8c6df4b4443bf3f5215e1
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-05-13 16:30:47 -0300

    Feat: Add interface MovementeLogService

--- src/main/java/com/zup/gestaodeativos/services/MovementLogService.java
+++ src/main/java/com/zup/gestaodeativos/services/MovementLogService.java
@@ -1,4 +1,10 @@
 package com.zup.gestaodeativos.services;
 
+import com.zup.gestaodeativos.models.MovementLog;
+
+import java.util.List;
+
 public interface MovementLogService {
+    MovementLog registerMovement(MovementLog log);
+    List<MovementLog> listAll();
 }
