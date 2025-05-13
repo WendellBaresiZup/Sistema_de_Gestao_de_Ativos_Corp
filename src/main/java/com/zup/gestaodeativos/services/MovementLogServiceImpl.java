ccb5aaed25119f8ba1cc85b01e67625ed89c68c6
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-05-13 16:20:01 -0300

    Feat: Add MovementeLogServiceImpl to work with business logic

--- src/main/java/com/zup/gestaodeativos/services/MovementLogServiceImpl.java
+++ src/main/java/com/zup/gestaodeativos/services/MovementLogServiceImpl.java
@@ -1,4 +1,24 @@
 package com.zup.gestaodeativos.services;
 
-public class MovementLogService {
+import com.zup.gestaodeativos.models.MovementLog;
+import com.zup.gestaodeativos.repository.MovementLogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class MovementLogServiceImpl implements MovementLogService{
+    @Autowired
+    private MovementLogRepository repository;
+
+    public MovementLog registerMovement(MovementLog log){
+        log.setMovementDate(LocalDateTime.now());
+        return repository.save(log);
+    }
+
+    public List<MovementLog> listAll(){
+        return repository.findAll();
+    }
 }
