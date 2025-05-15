e3e7367cba6951a23d000c4379207811251fc20b
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-05-15 13:23:30 -0300

    Refactor: Alter class LogMovimentacaoResponseDTO for output data

--- src/main/java/com/zup/gestaodeativos/dto/MovementLogRequestDTO.java
+++ src/main/java/com/zup/gestaodeativos/dto/MovementLogRequestDTO.java
@@ -1,16 +1,15 @@
 package com.zup.gestaodeativos.dto;
 
-import lombok.Data;
+import lombok.*;
 
 @Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class MovementLogRequestDTO {
     private Long activeId;
     private Long userId;
     private String action;
 
-    public MovementLogRequestDTO(Long activeId, Long userId, String action){
-        this.activeId = activeId;
-        this.userId = userId;
-        this.action = action;
-    }
 }
