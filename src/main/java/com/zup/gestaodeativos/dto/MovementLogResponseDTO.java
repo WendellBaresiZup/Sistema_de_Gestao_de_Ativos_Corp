6041c48f41076c1e9adec3392be7a04e43d16fa1
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-05-15 13:24:12 -0300

    Refactor: Alter class LogMovimentacaoResponseDTO for output data

--- src/main/java/com/zup/gestaodeativos/dto/MovementLogResponseDTO.java
+++ src/main/java/com/zup/gestaodeativos/dto/MovementLogResponseDTO.java
@@ -1,28 +1,20 @@
 package com.zup.gestaodeativos.dto;
 
-import com.zup.gestaodeativos.models.Active;
-import com.zup.gestaodeativos.models.User;
-import lombok.Data;
+
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class MovementLogResponseDTO {
     private Long id;
-    private Active active;
-    private User user;
+    private ActiveResponseDTO activeResponseDTO;
+    private UserResponseDTO userResponseDTO;
     private LocalDateTime movementDate;
     private String action;
 
-    public MovementLogResponseDTO (Long id, Active active, User user, LocalDateTime movementDate, String action){
-        this.id = id;
-        this.active = active;
-        this.user = user;
-        this.movementDate = movementDate;
-        this.action = action;
-    }
-
-    public MovementLogResponseDTO(){
-
-    }
 }
