624af04b8dc068f5ea2b53432bc95810465a522b
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-05-15 13:25:08 -0300

    Refactor: Alter class MovementeLogServiceImpl

--- src/main/java/com/zup/gestaodeativos/services/MovementLogServiceImpl.java
+++ src/main/java/com/zup/gestaodeativos/services/MovementLogServiceImpl.java
@@ -1,7 +1,11 @@
 package com.zup.gestaodeativos.services;
 
+import com.zup.gestaodeativos.dto.MovementLogRequestDTO;
+import com.zup.gestaodeativos.models.Active;
 import com.zup.gestaodeativos.models.MovementLog;
+import com.zup.gestaodeativos.repository.ActiveRepository;
 import com.zup.gestaodeativos.repository.MovementLogRepository;
+import com.zup.gestaodeativos.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,15 +14,14 @@ import java.util.List;
 
 @Service
 public class MovementLogServiceImpl implements MovementLogService{
-    @Autowired
-    private MovementLogRepository repository;
 
-    public MovementLog registerMovement(MovementLog log){
-        log.setMovementDate(LocalDateTime.now());
-        return repository.save(log);
+    @Override
+    public MovementLog registerMovement(MovementLog log) {
+        return null;
     }
 
-    public List<MovementLog> listAll(){
-        return repository.findAll();
+    @Override
+    public List<MovementLog> listAll() {
+        return List.of();
     }
 }
